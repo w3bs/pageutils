d1500775740a48abea70219d7c15e089ea56df73
w3bs <w3bs.utils@gmail.com>
2021-07-21 03:31:40 +0200

    exports

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 console.log('index.js starting');
-const open = require('./lib/openpage.js')
-const click = require('./lib/clickelement.js')
+const open = require('./lib/openpage/openpage.js')
+const click = require('./lib/clickelement/clickelement.js')
 console.log('\n in index, open.done = %j, click.done = %j', open.done, click.done)
 
 module.exports.pageutils = 'pageutils';
